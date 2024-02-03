651b9dd67563b1dff1bd90b4167d63d8c9a48e1f
Ahmed Hamed <ahmedhrayyan@outlook.com>
2024-02-03 02:03:32 +0200

    feat: add router logic

---
+++ src/routes/Home/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="py-20 text-center">
+      <h1 className="text-3xl mb-2">Welcome to the home page</h1>
+      <p>Currently under development</p>
+    </main>
+  );
+}
